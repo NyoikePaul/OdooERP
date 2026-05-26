5a292e391fcca880d437d15606092659d63445f4
NyoikePaul <nyoikepaul2@gmail.com>
2026-05-26 13:54:17 +0300

    feat: add price validation constraint

--- real_estate_management/models/property.py
+++ real_estate_management/models/property.py
@@ -7,3 +7,4 @@ class RealEstateProperty(models.Model):
 
     name = fields.Char(required=True, tracking=True)
     state = fields.Selection([('available', 'Available')], default='available', tracking=True)
+_sql_constraints = [('check_price', 'CHECK(price > 0)', 'Price must be positive')]
