3e9f55766c3cb004884337100a8e020899d43116
NyoikePaul <nyoikepaul2@gmail.com>
2026-05-29 10:40:30 +0300

    feat(types): property type, tag, amenity models with SQL constraints and sequence ordering

--- custom_addons/kenya_real_estate/models/property_type.py
+++ custom_addons/kenya_real_estate/models/property_type.py
@@ -2,42 +2,45 @@ from odoo import models, fields, api
 
 
 class EstatePropertyType(models.Model):
-    _name = 'estate.property.type'
+    _name        = 'estate.property.type'
     _description = 'Property Type'
-    _order = 'sequence, name'
+    _order       = 'sequence, name'
 
-    name       = fields.Char("Type", required=True, translate=True)
-    sequence   = fields.Integer("Sequence", default=10)
-    color      = fields.Integer("Color Index")
-    icon       = fields.Char("Icon", default='fa-building')
+    name         = fields.Char("Type", required=True, translate=True)
+    sequence     = fields.Integer("Sequence", default=10)
     property_ids = fields.One2many('estate.property', 'property_type_id', string="Properties")
-    property_count = fields.Integer(compute='_compute_property_count', string="Properties")
+    property_count = fields.Integer(compute='_compute_count', store=True)
+    active       = fields.Boolean(default=True)
+
+    _sql_constraints = [('name_unique', 'UNIQUE(name)', 'Property type must be unique.')]
 
     @api.depends('property_ids')
-    def _compute_property_count(self):
+    def _compute_count(self):
         for rec in self:
             rec.property_count = len(rec.property_ids)
 
 
 class EstatePropertyTag(models.Model):
-    _name = 'estate.property.tag'
+    _name        = 'estate.property.tag'
     _description = 'Property Tag'
-    _order = 'name'
-
-    name  = fields.Char("Tag", required=True, translate=True)
-    color = fields.Integer("Color Index")
+    _order       = 'name'
+    name         = fields.Char("Tag", required=True, translate=True)
+    color        = fields.Integer("Color Index", default=0)
+    _sql_constraints = [('name_unique', 'UNIQUE(name)', 'Tag must be unique.')]
 
 
 class EstateAmenity(models.Model):
-    _name = 'estate.amenity'
+    _name        = 'estate.amenity'
     _description = 'Property Amenity'
-    _order = 'name'
+    _order       = 'sequence, name'
 
-    name     = fields.Char("Amenity", required=True, translate=True)
-    icon     = fields.Char("FA Icon", default='fa-check')
-    category = fields.Selection([
-        ('security', 'Security'),
-        ('leisure',  'Leisure'),
-        ('utility',  'Utility'),
-        ('transport','Transport'),
+    name         = fields.Char("Amenity", required=True, translate=True)
+    sequence     = fields.Integer(default=10)
+    category     = fields.Selection([
+        ('utility',   'Utility'),
+        ('security',  'Security'),
+        ('leisure',   'Leisure'),
+        ('transport', 'Transport'),
     ], default='utility')
+    icon         = fields.Char("FA Icon", default='fa-check')
+    active       = fields.Boolean(default=True)
